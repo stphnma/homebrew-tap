f0aaf40a0cc04aca8f0b1efd22a91c820986efd0
Stephen Ma <stphnma@gmail.com>
2023-03-30 16:05:02 -0400

    initial commit

---
+++ Formula/s3_tui.rb
@@ -0,0 +1,11 @@
+class Tle < Formula
+    desc "CLI for navigating AWS S3"
+    homepage "https://github.com/stphnma/s3_tui"
+    url "https://github.com/stphnma/s3_tui/releases/download/0.1.11/s3_tui.tar.gz"
+    sha256 "9bf8a376565baa2bf40941b1f3599dd1333ac6a9"
+    version "0.1.11"
+  
+    def install
+      bin.install "s3_tui"
+    end
+end
